5853fd1bebd50be417d76798b72a98edd7c8b276
Christopher Su <christophersu9@gmail.com>
2014-05-17 12:48:21 -0700

    atom, vim, update script

---
+++ update.sh
@@ -0,0 +1,6 @@
+cp -rf /Users/christophersu/Library/Application\ Support/Sublime\ Text\ 3/Packages/User sublime_text_3
+cp -rf /Users/christophersu/.bashrc .
+cp -rf /Users/christophersu/.bash_profile .
+cp -rf /Users/christophersu/.fortran .
+cp -rf /Users/christophersu/.vim vim
+cp -rf /Users/christophersu/.atom atom
