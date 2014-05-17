e93a4aa24cdfe7f7beb512d0f63155240717ce59
Christopher Su <christophersu9@gmail.com>
2014-05-17 12:49:59 -0700

    cleanup

--- update.sh
+++ update.sh
@@ -2,5 +2,6 @@ cp -rf /Users/christophersu/Library/Application\ Support/Sublime\ Text\ 3/Packag
 cp -rf /Users/christophersu/.bashrc .
 cp -rf /Users/christophersu/.bash_profile .
 cp -rf /Users/christophersu/.fortran .
+cp -rf /Users/christophersu/.slate .
 cp -rf /Users/christophersu/.vim vim
 cp -rf /Users/christophersu/.atom atom
