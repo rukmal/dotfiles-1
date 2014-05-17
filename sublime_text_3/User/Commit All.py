b6ba3720bafc5803a2ee53552d03aead56009a4a
Christopher Su <christophersu9@gmail.com>
2014-05-17 14:45:45 -0700

    fix

--- sublime_text_3/User/Commit All.py
+++
@@ -1,7 +0,0 @@
-import sublime
-import sublime_plugin
-from git import GitTextCommand
-
-class GitCommitAllCommand(GitTextCommand):
-    def run(self, edit):
-        self.run_command(['git', 'commit', '-a', '-m', 'External editor'])
