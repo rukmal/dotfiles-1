b5612fbab1494922e9ec755d061dcb35697cabff
Christopher Su <christophersu9@gmail.com>
2014-09-25 00:32:02 -0700

    mac config

--- mac/mac_config.sh
+++ mac/mac_config.sh
@@ -1,4 +1,9 @@
+# make dock animations faster
 defaults write com.apple.Dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0.7
-
 killall Dock
+
+# set screenshot configuration
+defaults write com.apple.screencapture location /Users/csu/SkyDrive/Screenshots
+defaults write com.apple.screencapture type jpg
+killall SystemUIServer
