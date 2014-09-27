616b4efd8ba1c3895fb9642b6186326e24303964
Christopher Su <christophersu9@gmail.com>
2014-09-27 11:58:06 -0700

    mac

--- mac/mac_config.sh
+++ mac/mac_config.sh
@@ -4,9 +4,12 @@ defaults write com.apple.dock autohide-time-modifier -float 0.7
 killall Dock
 
 # set screenshot configuration
-defaults write com.apple.screencapture location /Users/christophersu/SkyDrive/Screenshots
+defaults write com.apple.screencapture location /Users/christophersu/OneDrive/Screenshots
 defaults write com.apple.screencapture type jpg
 killall SystemUIServer
 
 # remove hold down letter, accent menu
 defaults write -g ApplePressAndHoldEnabled -bool false
+
+# faster key repeat
+defaults write NSGlobalDomain KeyRepeat -int 0
