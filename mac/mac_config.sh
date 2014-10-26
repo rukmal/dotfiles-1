746d0f18acd3aefd1ed42b763cb9f52ee92d3f66
Christopher Su <christophersu9@gmail.com>
2014-10-26 13:06:54 -0700

    mac config

--- mac/mac_config.sh
+++ mac/mac_config.sh
@@ -1,12 +1,10 @@
 # make dock animations faster
 defaults write com.apple.Dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0.7
-killall Dock
 
 # set screenshot configuration
 defaults write com.apple.screencapture location /Users/christophersu/Dropbox/Screenshots
 defaults write com.apple.screencapture type jpg
-killall SystemUIServer
 
 # remove hold down letter, accent menu
 defaults write -g ApplePressAndHoldEnabled -bool false
@@ -16,3 +14,7 @@ defaults write NSGlobalDomain KeyRepeat -int 0
 
 # disable mouse acceleration
 defaults write .GlobalPreferences com.apple.mouse.scaling -1
+
+# restart system services for changes to take effect
+killall Dock
+killall SystemUIServer
